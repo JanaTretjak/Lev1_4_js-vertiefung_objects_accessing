2626f50bc1d06240c816b00a5b9de8a44fec10c2
JanaTretjak <tretiachechka@gmail.com>
2020-03-16 14:12:09 +0100

    ver 0

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let unsereHaustiere = [{ tiertyp: "Katze", names: ["Gipsy", "Nala", "Dinky"] },
+{ tiertyp: "Hunde", names: ["Knöpfchen", "Pinselchen", "Droopy"] }];
+console.log(unsereHaustiere[0].names[1])
+console.log(unsereHaustiere[1].names[2])
+console.log(unsereHaustiere[1].names)
+
+unsereHaustiere[1].names = ["Sharik", "Reks", "Maja"]
+console.log(unsereHaustiere[1].names)
